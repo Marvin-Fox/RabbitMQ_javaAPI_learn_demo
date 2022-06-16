a4b6b924f20c0d190fff4e5fcccc8e48624b130a
Marvin-Fox <275836684@qq.com>
2022-06-16 15:37:34 +0800

    confirm消息确认

--- src/main/java/com/mmr/rabbitmq08/confirm/addConfirmListener/SendSingle.java
+++ src/main/java/com/mmr/rabbitmq08/confirm/addConfirmListener/SendSingle.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 /**
- * 单条发送，addConfirmListener异步监听确认
+ * 单条发送【异步监听确认】
+ * addConfirmListener：confirm确认：当消息发送到broker服务器交换机【exchange】时，该方法被调用.
+ * addReturnListener：return确认：当消息从交换机（Exchange）路由到队列（Queue）失败时，该方法被调用【若成功，则不调用】
  */
 public class SendSingle {
 	private static final String ROUTING_KEY = "confirm.addConfirmListener";
@@ -19,6 +21,9 @@ public class SendSingle {
 		String msg="hello confrom message【single addConfirmListener】";
 		channel.basicPublish(RecvConfirm.EXCHANGE_NANE, ROUTING_KEY, null, msg.getBytes());
 		//异步监听确认是否发送成功
+		/**
+		 * 回调函数: confirm确认：当消息发送到broker服务器交换机【exchange】时，该方法被调用.
+		 */
 		channel.addConfirmListener(new ConfirmListener() {
 			/**
 			 * 消息成功处理
@@ -26,6 +31,7 @@ public class SendSingle {
 			 * @param multiple:是否批量
 			 * @throws IOException
 			 */
+			@Override
 			public void handleAck(long deliveryTag, boolean multiple) throws IOException {
 				System.out.println("message send success【single addConfirmListener】:deliveryTag【"+ deliveryTag +"】multiple【"+ multiple +"】");
 			}
@@ -36,21 +42,26 @@ public class SendSingle {
 			 * @param multiple:是否批量
 			 * @throws IOException
 			 */
+            @Override
 			public void handleNack(long deliveryTag, boolean multiple) throws IOException {
 				System.out.println("message send failed【single addConfirmListener】:deliveryTag【"+ deliveryTag +"】multiple【"+ multiple +"】");
 			}
 		});
 
-//		channel.addReturnListener(new ReturnListener() {
-//			public void handleReturn(int i, String s, String s1, String s2, AMQP.BasicProperties basicProperties, byte[] bytes) throws IOException {
-//				//如果交换机分发消息到队列失败，则会执行此方法（用来处理交换机分发消息到队列失败的情况）
-//				System.out.println("*****"+i);  //标识
-//				System.out.println("*****"+s);  //
-//				System.out.println("*****"+s1); //交换机名
-//				System.out.println("*****"+s2); //交换机对应的队列的key
-//				System.out.println("*****"+new String(bytes));  //发送的消息
-//			}
-//		});
+		/**
+		 * 回调函数: return确认：当消息从交换机（Exchange）路由到队列（Queue）失败时，该方法被调用【若成功，则不调用】
+		 */
+		channel.addReturnListener(new ReturnListener() {
+		    @Override
+			public void handleReturn(int i, String s, String s1, String s2, AMQP.BasicProperties basicProperties, byte[] bytes) throws IOException {
+				//如果交换机分发消息到队列失败，则会执行此方法（用来处理交换机分发消息到队列失败的情况）
+				System.out.println("*****"+i);  //标识
+				System.out.println("*****"+s);  //
+				System.out.println("*****"+s1); //交换机名
+				System.out.println("*****"+s2); //交换机对应的队列的key
+				System.out.println("*****"+new String(bytes));  //发送的消息
+			}
+		});
 		//给监听一点时间再关闭
 		Thread.sleep(5000);
 		channel.close();
